220f6d5ea4833e873a3ad50c89a82909471be171
laurszalk <laurenszalk@gmail.com>
2023-04-18 13:04:40 -0400

    updated sw js file and added README

---
+++ README.md
@@ -0,0 +1,17 @@
+# Module 19 PWA Text Editor
+
+## Description
+
+A single-page text editor application that runs in the browser. It features several data persistence techniques in case one of the options is not supported by the browser. The app also functions offline.
+
+## Deployment
+
+## Screenshot
+
+## Technologies
+
+HTML, CSS, JavaScript, Node.js, Express.js, IndexedDB, Babel, Webpack, Workbox, Idb, Concurrently, Heroku
+
+## License
+
+This project is licensed under the MIT License.
--- client/src-sw.js
+++ client/src-sw.js
@@ -1,14 +1,14 @@
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
-const { registerRoute } = require('workbox-routing');
-const { CacheableResponsePlugin } = require('workbox-cacheable-response');
-const { ExpirationPlugin } = require('workbox-expiration');
-const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+const { offlineFallback, warmStrategyCache } = require("workbox-recipes");
+const { CacheFirst } = require("workbox-strategies");
+const { registerRoute } = require("workbox-routing");
+const { CacheableResponsePlugin } = require("workbox-cacheable-response");
+const { ExpirationPlugin } = require("workbox-expiration");
+const { precacheAndRoute } = require("workbox-precaching/precacheAndRoute");
 
 precacheAndRoute(self.__WB_MANIFEST);
 
 const pageCache = new CacheFirst({
-  cacheName: 'page-cache',
+  cacheName: "page-cache",
   plugins: [
     new CacheableResponsePlugin({
       statuses: [0, 200],
@@ -20,11 +20,28 @@ const pageCache = new CacheFirst({
 });
 
 warmStrategyCache({
-  urls: ['/index.html', '/'],
+  urls: ["/index.html", "/"],
   strategy: pageCache,
 });
 
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+registerRoute(({ request }) => request.mode === "navigate", pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  // The callback function that filters the requests we want to cache (JS and CSS files)
+  ({ request }) => ["style", "script", "worker"].includes(request.destination),
+  new StaleWhileRevalidate({
+    // Name of the cache storage
+    cacheName: "asset-cache",
+    plugins: [
+      // Plugin will cache responses with these headers to a max age of 30 days
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  })
+);
