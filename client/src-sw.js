5ff153986eb44eca782482c4e1b40b4dbab152d7
laurszalk <laurenszalk@gmail.com>
2023-04-18 20:20:07 -0400

    fixed bug in sw.js file

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,5 +1,5 @@
 const { offlineFallback, warmStrategyCache } = require("workbox-recipes");
-const { CacheFirst } = require("workbox-strategies");
+const { CacheFirst, StaleWhileRevalidate } = require("workbox-strategies");
 const { registerRoute } = require("workbox-routing");
 const { CacheableResponsePlugin } = require("workbox-cacheable-response");
 const { ExpirationPlugin } = require("workbox-expiration");
@@ -34,7 +34,7 @@ registerRoute(
     // Name of the cache storage
     cacheName: "asset-cache",
     plugins: [
-      // Plugin will cache responses with these headers to a max age of 30 days
+      // Plugin will cache responses with these headers to max age of 30 days
       new CacheableResponsePlugin({
         statuses: [0, 200],
       }),
